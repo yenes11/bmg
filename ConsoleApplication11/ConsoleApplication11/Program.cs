45144917c92980e4a5c22b7aa1193650db26598c
yenes11 <34625410+yenes11@users.noreply.github.com>
2017-12-17 17:45:42 +0300

    Add files via upload

---
+++ ConsoleApplication11/ConsoleApplication11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad; int e, y;
+            Console.WriteLine("Adınızı giriniz=");
+            ad = Console.ReadLine();
+            e = ad.Length;
+            for (int i = 0; i < e; i++)
+            {
+                y = (int)ad[i] + 10;
+                Console.Write((char)y);
+            }
+            Console.ReadKey();
+          
+        }
+    }
+}
